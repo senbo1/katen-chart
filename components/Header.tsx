9797019741c7aa1599daf7768ea80b8529acd960
senbo1 <harshbhadu86@gmail.com>
2025-01-07 12:52:26 +0530

    added basic header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,16 @@
-import { FC } from 'react';
+import { Tv } from 'lucide-react';
 
-type HeaderProps = {};
-
-const Header: FC<HeaderProps> = () => {
-  return <header className="p-4 border-2">Header</header>;
+const Header = () => {
+  return (
+    <header className="p-4 sticky inset-0 top-0 z-10 backdrop-blur-md">
+      <nav className="flex justify-center items-center">
+        <div className="flex items-center gap-2">
+          <Tv className="w-6 h-6 mr-2" />
+          <h1 className="text-xl font-bold">加点 chart</h1>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
