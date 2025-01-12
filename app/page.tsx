3c1af6e3996e7381bd9b644b96ef9f7142203bc7
senbo1 <harshbhadu86@gmail.com>
2025-01-12 22:04:06 +0530

    added animes fetched from jikan api

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import List from '@/components/AnimeList/List';
 import Header from '@/components/Header';
+
+export const revalidate = 3600;
 
 export default function Home() {
   return (
     <main>
       <Header />
+
+      <List />
     </main>
   );
 }
--- lib/anime.ts
+++ lib/anime.ts
@@ -1,20 +1,35 @@
 import axios, { AxiosResponse } from 'axios';
 import { Anime, AnimeResponse } from './types';
+// import { delay } from './utils';
 
-export const getAnimeShows = async (): Promise<Anime[]> => {
-  let animeList: Anime[] = [];
+export const getAnimeList = async (): Promise<Anime[]> => {
+  const animeList: Anime[] = [];
   let page = 1;
   let hasNextPage = true;
+  const seenIds = new Set<number>();
 
   while (hasNextPage) {
     const res: AxiosResponse<AnimeResponse> = await axios.get(
       `https://api.jikan.moe/v4/seasons/now?page=${page}`
     );
     const { data: anime } = res.data;
-    animeList = animeList.concat(anime);
+
+    anime.forEach((show) => {
+      if (!seenIds.has(show.mal_id)) {
+        animeList.push(show);
+        seenIds.add(show.mal_id);
+      }
+    });
 
     hasNextPage = res.data.pagination.has_next_page;
     page++;
+
+    // if (page % 2 === 0) {
+    //   await delay(1000);
+    // }
+    if (page > 2) {
+      break;
+    }
   }
 
   return animeList;
