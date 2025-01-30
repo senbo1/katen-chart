fc59c5e17397e8481dbc292f7f5e95483ce9c78e
senbo1 <harshbhadu86@gmail.com>
2025-01-31 00:48:14 +0530

    fix card overlap css

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className="w-full mx-auto flex flex-col items-center p-2 xl:p-4 my-16">
+      <div className="w-full mx-auto flex flex-col items-center p-2 xl:p-0 my-16">
         <HeroLine />
         <List />
       </div>
